dfdd62411cdfa9d4b81aacbd23b826480104d296
paulrfirefly <paulraciszbusiness@gmail.com>
2023-04-26 11:27:20 -0400

    added comment Co-authored-by: Paulracisz <paulieracisz@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,8 @@ public class Main {
         if (seconds <= 0 || seconds >= 59) {
             // input validation
             return "Invalid data for seconds (" + seconds + "), must be 0 and 59";
-        }
+        } 
+        // calculate hours
         int hours = minutes / 60;
         int remainingMinutes = minutes % 60;
         return hours + "h " + remainingMinutes + "m " + seconds + "s";
